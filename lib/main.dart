7f361634331192884e12ddb74642bf5e992ddae9
Lucas Correa <lucascorreanasarioprogramador@gmail.com>
2023-08-15 20:11:12 -0300

    feat/tarefa-03

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,42 @@ class MyApp extends StatefulWidget {
     return Container(
       color: Colors.yellow,
       margin: const EdgeInsets.all(3),
-      width: 100,
-      height: 50,
+      width: 50,
+      height: 200,
     );
   }
+  
 
   containerBuild1() {
     return Container(
       color: Colors.red,
       margin: const EdgeInsets.all(3),
-      width: 200,
-      height: 50,
+      width: 50,
+      height: 300,
     );
   }
     containerBuild2() {
     return Container(
       color: Colors.green,
       margin: const EdgeInsets.all(3),
-      width: 300,
-      height: 50,
+      width: 50,
+      height: 400,
     );
   }
    containerBuild3() {
     return Container(
       color: Colors.blue,
       margin: const EdgeInsets.all(3),
-      width: 400,
-      height: 50,
+      width: 50,
+      height: 500,
+    );
+  }
+   containerBuild4() {
+    return Container(
+      color: Colors.grey,
+      margin: const EdgeInsets.all(3),
+      width: 50,
+      height: 600,
     );
   }
 
@@ -51,12 +60,14 @@ class _MyAppState extends State<MyApp> {
      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text("AppBar")),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: 
-          [containerBuild(), containerBuild1(), containerBuild2(), containerBuild3()],
+         body: Container(
+          alignment: Alignment.bottomLeft,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [containerBuild(), containerBuild1(), containerBuild2(), containerBuild3(), containerBuild4()],
         ),
+      )
       )
     );
     
